d0a57f701964c0cfd6d709c04c26066e2087d2fb
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-04-01 22:53:32 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,12 @@
-
+class Game:
+    def __init__(self, id):
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+        self.id = id
+        self.moves =[None, None]
+        self.wins = [0,0]
+        self.ties = 0
     def get_player_move(self, p):
         """
         :param p: [0, 1]
